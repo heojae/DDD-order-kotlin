98cccc1413e0d59953e25c16241d14a9a267dba1
heojae <shrldh3576@naver.com>
2021-11-11 22:42:38 +0900

    item API 까지 구현 성공

--- src/main/kotlin/com/example/hello/interfaces/item/ItemApiController.kt
+++ src/main/kotlin/com/example/hello/interfaces/item/ItemApiController.kt
@@ -1,15 +1,44 @@
 package com.example.hello.interfaces.item
 
 import com.example.hello.application.item.ItemFacade
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.example.hello.common.response.CommonResponse
+import com.example.hello.domain.item.ItemCommand
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/v1/items")
 class ItemApiController(
     private val itemFacade: ItemFacade,
-//    private val itemDtoMapper: ItemDtoMapper
+    private val itemDtoMapper: ItemDtoMapper
 ) {
+    @PostMapping
+    fun registerItem(@RequestBody @Valid request: ItemDto.RegisterItemRequest): CommonResponse<ItemDto.RegisterResponse> {
+        val partnerToken: String = request.partnerToken
+        val itemCommand: ItemCommand.RegisterItemRequest = itemDtoMapper.of(request)
+        val itemToken = itemFacade.registerItem(request = itemCommand, partnerToken = partnerToken)
+        val response = itemDtoMapper.of(itemToken)
+        return CommonResponse.success(response)
+    }
 
+    @PostMapping("/change-on-sales")
+    fun changeOnSale(@RequestBody @Valid request: ItemDto.ChangeStatusItemRequest): CommonResponse<String> {
+        val itemToken = request.itemToken
+        itemFacade.changeOnSaleItem(itemToken)
+        return CommonResponse.success("OK")
+    }
+
+    @PostMapping("/change-end-of-sales")
+    fun changeEndOfSale(@RequestBody @Valid request: ItemDto.ChangeStatusItemRequest): CommonResponse<String> {
+        val itemToken = request.itemToken
+        itemFacade.changeEndOfSale(itemToken)
+        return CommonResponse.success("OK")
+    }
+
+    @GetMapping("/{itemToken}")
+    fun retrieve(@PathVariable("itemToken") itemToken: String): CommonResponse<ItemDto.Main> {
+        val itemInfo = itemFacade.retrieveItemInfo(itemToken)
+        val response = itemDtoMapper.of(itemInfo)
+        return CommonResponse.success(response)
+    }
 }
--- src/main/kotlin/com/example/hello/interfaces/item/ItemDto.kt
+++ src/main/kotlin/com/example/hello/interfaces/item/ItemDto.kt
@@ -1,32 +1,43 @@
 package com.example.hello.interfaces.item
 
 import com.example.hello.domain.item.Item
+import javax.validation.constraints.NotBlank
 
 class ItemDto {
     data class RegisterItemRequest(
+        @field:NotBlank(message = "empty partnerToken")
         val partnerToken: String,
+        @field:NotBlank(message = "empty itemName")
         val itemName: String,
+        @field:NotBlank(message = "empty itemPrice")
         val itemPrice: Long,
         val itemOptionGroupList: List<RegisterItemOptionGroupRequest>
     )
 
     data class RegisterItemOptionGroupRequest(
+        @field:NotBlank(message = "empty ordering")
         val ordering: Int,
+        @field:NotBlank(message = "empty itemOptionGroupName")
         val itemOptionGroupName: String,
         val itemOptionList: List<RegisterItemOptionRequest>
     )
 
     data class RegisterItemOptionRequest(
+        @field:NotBlank(message = "empty ordering")
         val ordering: Int,
+        @field:NotBlank(message = "empty itemOptionName")
         val itemOptionName: String,
+        @field:NotBlank(message = "empty itemOptionPrice")
         val itemOptionPrice: Long
     )
 
     data class RegisterResponse(
+        @field:NotBlank(message = "empty itemToken")
         val itemToken: String
     )
 
     data class ChangeStatusItemRequest(
+        @field:NotBlank(message = "empty itemToken")
         val itemToken: String
     )
 
